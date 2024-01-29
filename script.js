9b332e8f386d57087b3716c495821ba9cdc3b0d7
MaitreRenard18 <maitre.renardowo@gmail.com>
2024-01-29 21:15:08 +0100

    Animations

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World!")
+document.addEventListener("DOMContentLoaded", function() {
+    var redAmbilight = document.getElementById("red-ambilight");
+    var purpleAmbilight = document.getElementById("purple-ambilight");
+
+    function randomMovement() {
+        var x = Math.random() * 100;
+        var y = Math.random() * 100;
+
+        redAmbilight.style.left = x + "%";
+        redAmbilight.style.top = y + "%";
+
+        purpleAmbilight.style.left = 100 - x + "%";
+        purpleAmbilight.style.top = 100 - y + "%";
+    }
+
+    function moveAmbilight() {
+        randomMovement();
+    }
+    
+    randomMovement();
+    setInterval(moveAmbilight, 10000);
+});
